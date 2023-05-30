bdea2df62fcf5e4dc3eb6d8147a3433353c2610f
shankarhcl <shankar.sharma8920@gmail.com>
2023-05-30 21:37:23 +0530

    3 MS base created

---
+++ JavaBrainsMicroservice/moviecatalogservice/src/test/java/com/javabrains/moviecatalogservice/MoviecatalogserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabrains.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecatalogserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
