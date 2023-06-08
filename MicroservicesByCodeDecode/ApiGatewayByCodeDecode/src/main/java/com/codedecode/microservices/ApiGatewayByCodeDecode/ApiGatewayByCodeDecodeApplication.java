4d685cff938d247694a5cfc447be209c99977d6a
shankarhcl <shankar.sharma8920@gmail.com>
2023-06-08 14:06:20 +0530

    api gateway

---
+++ MicroservicesByCodeDecode/ApiGatewayByCodeDecode/src/main/java/com/codedecode/microservices/ApiGatewayByCodeDecode/ApiGatewayByCodeDecodeApplication.java
@@ -0,0 +1,13 @@
+package com.codedecode.microservices.ApiGatewayByCodeDecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayByCodeDecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayByCodeDecodeApplication.class, args);
+	}
+
+}
---
+++ MicroservicesByCodeDecode/ApiGatewayByCodeDecode/src/test/java/com/codedecode/microservices/ApiGatewayByCodeDecode/ApiGatewayByCodeDecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedecode.microservices.ApiGatewayByCodeDecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayByCodeDecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
